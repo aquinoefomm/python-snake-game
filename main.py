8a87b89b34d498cee3a764bfb221ccd0504be92c
Everton de Aquino Peres <72323813+aquinoefomm@users.noreply.github.com>
2023-11-24 18:00:53 -0300

    high-score-tracking

--- main.py
+++ main.py
@@ -32,33 +32,16 @@ while game_is_on:
         snake.extend()
         scoreboard.incresa_score()
 
-    # Detect collisioin with wall:
+    # Detect collision with wall:
     if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
     # Detect collision with tail:
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 5:
-            game_is_on = False
-            scoreboard.game_over()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            scoreboard.reset()
+            snake.reset()
 
 
 screen.exitonclick()
